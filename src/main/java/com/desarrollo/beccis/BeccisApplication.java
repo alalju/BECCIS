415b6ccaafa45d0935bab674abe22f027caae6e0
alalju <alba.aj21@hotmail.com>
2024-01-10 08:23:05 -0600

    first commit

---
+++ src/main/java/com/desarrollo/beccis/BeccisApplication.java
@@ -0,0 +1,13 @@
+package com.desarrollo.beccis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeccisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeccisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desarrollo/beccis/BeccisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desarrollo.beccis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeccisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
